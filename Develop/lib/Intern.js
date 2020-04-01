48bee053aea55dc0063334266cc6a11bd3d5504a
JohnS <senakj2011@hotmail.com>
2020-03-31 22:56:55 -0400

    Defined and exported Intern class

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+class Intern {
+    constructor(internName, internId, internEmail, internSchool) {
+      if (!internName) {
+        throw new Error("Please enter your full name.");
+      }
+      if (!internId) {
+        throw new Error("Please your ID.");
+      }
+      if (!internEmail) {
+        throw new Error("Please enter your email.");
+      }
+      if (!internSchool) {
+        throw new Error("Please enter the school you attended.");
+      }
+      this.internName = internName;
+      this.internId = internId;
+      this.internEmail = internEmail;
+      this.internSchool = internSchool;
+    }
+  }
+  
+  printIntern();
+  
+  module.export = Intern;
