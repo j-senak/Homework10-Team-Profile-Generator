33fd158470831c127d424a15849a7f03b62ebc9b
JohnS <senakj2011@hotmail.com>
2020-03-31 23:01:18 -0400

    Defined and exported MAnager class

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+class Manager {
+    constructor(managerName, managerId, managerEmail, managerOfficeNumber) {
+      if (!managerName) {
+        throw new Error("Please enter your full name.");
+      }
+      if (!managerId) {
+        throw new Error("Please your ID.");
+      }
+      if (!managerEmail) {
+        throw new Error("Please enter your email.");
+      }
+      if (!managerOfficeNumber) {
+        throw new Error("Please enter your office number.");
+      }
+      this.managerName = managerName;
+      this.managerId = managerId;
+      this.managerEmail = managerEmail;
+      this.managerOfficeNumber = managerOfficeNumber;
+    }
+  }
+  
+  printManager();
+  
+  module.export = Manager;
